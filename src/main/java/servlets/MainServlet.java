f6a59f653aafe764e9796d9ceccf8a26df3b76e9
Maksim Remnev <remnev.maksim21@gmail.com>
2015-04-17 13:38:20 +0300

    create simple servlet

---
+++ src/main/java/servlets/MainServlet.java
@@ -0,0 +1,26 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * autor:maksim
+ * date: 17.04.15
+ * time: 13:20.
+ */
+public class MainServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter pw=resp.getWriter();
+        pw.print("it works");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
