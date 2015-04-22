c990887953626d62b67121343f5598096c6fdee1
Maksim Remnev <remnev.maksim21@gmail.com>
2015-04-22 12:20:39 +0300

    create simple jsp page

--- src/main/java/servlets/MainServlet.java
+++ src/main/java/servlets/MainServlet.java
@@ -6,19 +6,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  * autor:maksim
  * date: 17.04.15
  * time: 13:20.
  */
-@WebServlet(urlPatterns = "/")
+@WebServlet(urlPatterns = "/main")
 public class MainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter pw=resp.getWriter();
-        pw.print("it works");
     }
 
     @Override
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,21 @@
+<%@ page import="java.util.Date" %>
+<%@ page import="java.io.IOException" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: maksim
+  Date: 22.04.15
+  Time: 11:49
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
+<head>
+    <title>Test time jsp page</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+<form id="time" name="timepage">
+    <p>Date and time: <% out.print(new Date()); %></p>
+</form>
+
 </body>
 </html>
